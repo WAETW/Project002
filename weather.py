5250b9d9e65d57171cfe2e442a4f048e0c999ca7
Sean861026 <38836267+Sean861026@users.noreply.github.com>
2018-08-27 03:21:59 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,37 +4,38 @@ from bs4 import BeautifulSoup
 from googletrans import Translator
 from pprint import pprint
 
-translate = Translator()
-woeid = '2306179'
-res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-data = json.loads(res.text)
+def getweather():
+    translate = Translator()
+    woeid = '2306179'
+    res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    data = json.loads(res.text)
 
-data = data['query']['results']['channel']
+    data = data['query']['results']['channel']
 
-location = data['location']
+    location = data['location']
 
-wind = data['wind']
-speed = wind['speed']
+    wind = data['wind']
+    speed = wind['speed']
 
-atmosphere = data['atmosphere']
-humidity = atmosphere['humidity']
-visibility = atmosphere['visibility']
-pressure = atmosphere['pressure']
+    atmosphere = data['atmosphere']
+    humidity = atmosphere['humidity']
+    visibility = atmosphere['visibility']
+    pressure = atmosphere['pressure']
 
-astronomy = data['astronomy']
-sunrise = astronomy['sunrise']
-sunset = astronomy['sunset']
+    astronomy = data['astronomy']
+    sunrise = astronomy['sunrise']
+    sunset = astronomy['sunset']
 
-location = location['city']+' ,'+location['country']
-location = translate.translate(text=location,src='en',dest='zh-TW')
+    location = location['city']+' ,'+location['country']
+    location = translate.translate(text=location,src='en',dest='zh-TW')
 
-item = data['item']
-temp = item['condition']['temp']
-condition = translate.translate(text=item['condition']['text'],src='en',dest='zh-TW')
-date = translate.translate(text=item['condition']['date'],src='en',dest='zh-TW')
+    item = data['item']
+    temp = item['condition']['temp']
+    condition = translate.translate(text=item['condition']['text'],src='en',dest='zh-TW')
+    date = translate.translate(text=item['condition']['date'],src='en',dest='zh-TW')
 
-print(location.text)
-print('風速：' + speed +' 公里/小時')
-print('濕度：' + humidity + '%\n' + '能見度：' + visibility + '公里\n' + '氣壓：' + pressure + '毫巴')
-print('日出時間：' + sunrise + '\n日落時間；' + sunset)
-print('溫度：' + temp + '°C\n' + '天氣狀況：' + condition.text + '\n資料時間：' + date.text)
+    print(location.text)
+    print('風速：' + speed +' 公里/小時')
+    print('濕度：' + humidity + '%\n' + '能見度：' + visibility + '公里\n' + '氣壓：' + pressure + '毫巴')
+    print('日出時間：' + sunrise + '\n日落時間；' + sunset)
+    print('溫度：' + temp + '°C\n' + '天氣狀況：' + condition.text + '\n資料時間：' + date.text)
