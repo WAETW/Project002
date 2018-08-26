631a9a0c231508062b57dd67446d282353b8a2a0
Sean861026 <38836267+Sean861026@users.noreply.github.com>
2018-08-27 03:13:29 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,8 +1,10 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+from googletrans import Translator
 from pprint import pprint
 
+translate = Translator()
 woeid = '2306179'
 res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
 data = json.loads(res.text)
@@ -10,16 +12,29 @@ data = json.loads(res.text)
 data = data['query']['results']['channel']
 
 location = data['location']
+
 wind = data['wind']
+speed = wind['speed']
+
 atmosphere = data['atmosphere']
+humidity = atmosphere['humidity']
+visibility = atmosphere['visibility']
+pressure = atmosphere['pressure']
+
 astronomy = data['astronomy']
-item = data['item']
+sunrise = astronomy['sunrise']
+sunset = astronomy['sunset']
 
 location = location['city']+' ,'+location['country']
-wind = wind['speed']
+location = translate.translate(text=location,src='en',dest='zh-TW')
 
-print(location)
-print('風速：' + wind +' 公里/小時')
-print('濕度：' + atmosphere['humidity'] + '%\n' + '能見度：' + atmosphere['visibility'] + '公里\n' + '氣壓：' + atmosphere['pressure'] + '毫巴')
-print('日出時間：' + astronomy['sunrise'] + '\n日落時間；' + astronomy['sunset'])
-print('溫度：' + item['condition']['temp'] + '°C\n' + '天氣狀況：' + item['condition']['text'] + '\n資料時間：' + item['condition']['date'])
+item = data['item']
+temp = item['condition']['temp']
+condition = translate.translate(text=item['condition']['text'],src='en',dest='zh-TW')
+date = translate.translate(text=item['condition']['date'],src='en',dest='zh-TW')
+
+print(location.text)
+print('風速：' + speed +' 公里/小時')
+print('濕度：' + humidity + '%\n' + '能見度：' + visibility + '公里\n' + '氣壓：' + pressure + '毫巴')
+print('日出時間：' + sunrise + '\n日落時間；' + sunset)
+print('溫度：' + temp + '°C\n' + '天氣狀況：' + condition.text + '\n資料時間：' + date.text)
