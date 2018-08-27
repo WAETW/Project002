e9a2796de64f32a04be43e327617f704de3dc5d4
WAETW <zxc8110924@gmail.com>
2018-08-27 23:45:38 +0800

    = =

--- speech.py
+++ speech.py
@@ -1,13 +1,13 @@
 import speech_recognition as sr
 from Speak import speak
 import time
-def speech(title,duration,time):
+def speech(title,duration,i):
     r=sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration)
         print(title)
         speak(title,'zh-tw')
-        time.sleep(time)
+        time.sleep(i)
         audio=r.listen(source)
         try:
             recognize = r.recognize_google(audio, language="zh-TW")
