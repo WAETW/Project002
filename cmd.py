4a7cee8e42b9e0ec18b0c8d1037f0d7d3756de9f
WAETW <zxc8110924@gmail.com>
2018-08-28 00:01:01 +0800

    = =

--- cmd.py
+++ cmd.py
@@ -4,7 +4,7 @@ from weather import getweather
 from translate import *
 from News import *
 
-if playing_status() == "True":
+if playing_status() == True:
     spotifycontrol("暫停")
 command = speech("嗨!",5,1)
 if command == "音樂":
