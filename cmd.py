b9129ea74f2fad88347b11cc7fa34a2b93fc68a5
WAETW <zxc8110924@gmail.com>
2018-08-28 04:05:09 +0800

    123

--- cmd.py
+++ cmd.py
@@ -4,17 +4,27 @@ from weather import getweather
 from translate import *
 from News import *
 
+temp_status = False
 if playing_status() == True:
+    temp_status = playing_status()
     spotifycontrol("暫停")
-command = speech("嗨!",5,1)
+command = speech("嗨!",2,1)
 if command == "音樂":
-    subcommand = speech("請輸入指令:",5,2)
+    subcommand = speech("請輸入指令:",2,2)
     spotifycontrol(subcommand)
+    if temp_status == True:
+        resume()
 elif command == "翻譯":
-    translate(speech('請選擇要翻譯的語言',5,3), speech('正在翻譯...',5,2))
+    translate(speech('請選擇要翻譯的語言',2,3), speech('正在翻譯...',2,2))
+    if temp_status == True:
+        resume()
 elif command == "天氣":
     getweather()
+    if temp_status == True:
+        resume()
 elif command == "新聞":
-    post(speech('請選擇搜尋方式',5,2))
+    post(speech('請選擇搜尋方式',2,2))
+    if temp_status == True:
+        resume()
 else:
     print("...")
