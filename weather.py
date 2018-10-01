038baf9b4c06d85b2edc24c5e0b42974e316aa29
Sean861026 <38836267+Sean861026@users.noreply.github.com>
2018-10-01 18:29:24 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,70 +1,57 @@
 import requests
 import json
-from googletrans import Translator
-import speech_recognition
-from Speak import speak
 import time
+import datetime
+from speech_Bing import speech
+from BingTTS import TTS
 
-def getweather():
-    s = speech_recognition.Recognizer()
-    with speech_recognition.Microphone() as source:
-        s.adjust_for_ambient_noise(source, duration=5)
-        print('請說出要查詢的城市')
-        speak('請說出要查詢的城市','zh-tw')
-        time.sleep(2)
-        audio = s.listen(source)
-        a = s.recognize_google(audio, language='zh-TW')
-        city = '' + a
-    woeid = dict(台北='2306179',
-    新北='20070569',
-    基隆='2306188',
-    桃園='2298866',
-    新竹='2306185',
-    苗栗='2301128',
-    台中='2306181',
-    彰化='2306183',
-    南投='2306204',
-    雲林='2347346',
-    嘉義='2296315',
-    台南='2306182',
-    高雄='2306180',
-    屏東='2306189',
-    宜蘭='2306198',
-    花蓮='2306187',
-    台東='2306190')
-    translate = Translator()
+woeid = dict(臺北='2306179',
+新北='20070569',
+基隆='2306188',
+桃園='2298866',
+新竹='2306185',
+苗栗='2301128',
+臺中='2306181',
+彰化='2306183',
+南投='2306204',
+雲林='2347346',
+嘉義='2296315',
+臺南='2306182',
+高雄='2306180',
+屏東='2306189',
+宜蘭='2306198',
+花蓮='2306187',
+臺東='2306190')
+
+
+def weather_current():
+    city = speech("說出想要查詢的城市" ,3)
+    #load weather's json file
     res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid.get(city) + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
     data = json.loads(res.text)
 
     data = data['query']['results']['channel']
 
-    location = data['location']
-
-    wind = data['wind']
-    speed = wind['speed']
-
-    atmosphere = data['atmosphere']
-    humidity = atmosphere['humidity']
-    visibility = atmosphere['visibility']
-    pressure = atmosphere['pressure']
-
-    astronomy = data['astronomy']
-    sunrise = astronomy['sunrise']
-    sunset = astronomy['sunset']
-
-    location = location['city']+' ,'+location['country']
-    location = translate.translate(text=location,src='en',dest='zh-TW')
-
     item = data['item']
     temp = item['condition']['temp']
-    condition = translate.translate(text=item['condition']['text'],src='en',dest='zh-TW')
-    date = translate.translate(text=item['condition']['date'],src='en',dest='zh-TW')
 
-    #print(location.text)
-    #print('風速：' + speed +' 公里/小時')
-    #print('濕度：' + humidity + '%\n' + '能見度：' + visibility + '公里\n' + '氣壓：' + pressure + '毫巴')
-    #print('日出時間：' + sunrise + '\n日落時間；' + sunset)
-    #print('溫度：' + temp + '°C\n' + '天氣狀況：' + condition.text + '\n資料時間：' + date.text)
-    weather = '溫度：' + temp + '°C' + '濕度：' + humidity + '天氣狀況：' + condition.text
-    speak(weather , 'zh-tw')
-    time.sleep(10)
+    weather = '溫度：' + temp + '°C' + ' 濕度：' + humidity + '% 天氣狀況：' + item['condition']['text']
+    print(weather)
+    TTS(weather,'中文')
+
+def weather_forecast():
+    city = speech("說出想要查詢的城市",3)
+
+    #load weather's json file
+    res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid.get(city) + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')# + woeid.get(city))
+    data = json.loads(res.text)
+    forecast = data['query']['results']['channel']['item']['forecast']
+
+    #load timestamp
+    timestamp = datetime.datetime(2018,10,2)
+    timestamp = timestamp.strftime("%d %b %Y")
+    #print results
+    for i in range(len(forecast)) :
+        if timestamp == forecast[i]['date']:
+            print('氣溫：' + forecast[i]['high'] + '°C~' + forecast[i]['low'] + '°C 天氣狀況：' + forecast[i]['text'])
+            TTS('氣溫：' + forecast[i]['high'] + '°C~' + forecast[i]['low'] + '°C 天氣狀況：' + forecast[i]['text'],'中文')
