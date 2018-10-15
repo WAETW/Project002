6899bf0437a5962ddbf549694f1a688af1f8b022
WAETW <37590423+WAETW@users.noreply.github.com>
2018-10-16 00:21:10 +0800

    Update install.sh

--- Script/install.sh
+++ Script/install.sh
@@ -3,4 +3,4 @@ echo '環境變數設定中...'
 export SPOTIPY_CLIENT_ID='0303c0b3eaa14e7b9d3c49d270ab01e8'
 export SPOTIPY_CLIENT_SECRET='69cc45c34c1241f180a341cf822a2827'
 export SPOTIPY_REDIRECT_URI='http://localhost:8888/callback/'
-export GOOGLE_APPLICATION_CREDENTIALS="NewAgent-891dec01d538.json"
+export GOOGLE_APPLICATION_CREDENTIALS='/home/pi/demo/NewAgent-891dec01d538.json'
