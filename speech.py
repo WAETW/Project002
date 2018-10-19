52fc88444b5e0231ea1bf2fe6eaf5845dc4f7e7c
WAETW <zxc8110924@gmail.com>
2018-10-20 02:17:16 +0800

    remove useless file,change google speech api to bing

--- speech.py
+++ speech.py
@@ -1,5 +1,4 @@
 import speech_recognition as sr
-#from Speak import speak
 from BingTTS import TTS
 import time
 def speech(title,duration,i):
@@ -7,12 +6,11 @@ def speech(title,duration,i):
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration)
         print(title)
-        #speak(title,'zh-tw')
         TTS(title,'中文')
         time.sleep(i)
         audio=r.listen(source)
         try:
-            recognize = r.recognize_google(audio, language="zh-TW")
+            recognize = r.recognize_bing(audio,key="ca9c8dea98a74ef0bf98b5519a5010f3",language="zh-TW")
             print(recognize)
         except sr.UnknownValueError:
             recognize = "無法辨識!"
