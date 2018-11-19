9c13387951f7ebe6b78ddad26e6c5d7429319bc1
Ccpyo <charlie5566183@gmail.com>
2018-11-19 22:56:40 +0800

    fix dict error

--- translate.py
+++ translate.py
@@ -5,16 +5,20 @@ from BingTTS import TTS
 def translate(input,language_to):
     translate = Translator()
 
-    #say = '你好嗎'
-    result = translate.translate(input ,dest=lan.get(language_to))
-    #result = translate.translate(say ,dest=lan.get('日文'))
-    #result = translate.translate('我想吃晚餐',dest=lan.get('日文'))
+    if language_to in lan:
+        
+        #say = '你好嗎'
+        result = translate.translate(input ,dest=lan.get(language_to))
+        #result = translate.translate(say ,dest=lan.get('日文'))
+        #result = translate.translate('我想吃晚餐',dest=lan.get('日文'))
 
-    #speak(result.text,lan.get(l))
-    TTS(result.text,language_to)
-    #print (result.text)
-    return result.text
-
+        #speak(result.text,lan.get(l))
+        TTS(result.text,language_to)
+        #print (result.text)
+        return result.text
+    else:
+        
+        TTS('我不懂','中文')
 #翻譯語言
 lan = {
     '英文':'en',
@@ -26,5 +30,5 @@ lan = {
     '泰文':'th'
 }
 #translate(None)
-#translate('你好嗎','日文')
+translate('你好嗎','日文')
 #translate(speech('請選擇要翻譯的語言',2,3), speech('正在翻譯...',2,2))
