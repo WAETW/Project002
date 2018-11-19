24025b14c6d796a60c2e2f45a0ffca8cbbd83654
Ccpyo <charlie5566183@gmail.com>
2018-11-19 23:21:33 +0800

    fix dict error

--- weather.py
+++ weather.py
@@ -26,34 +26,39 @@ woeid = dict(臺北='2306179',
 
 
 def weather_current(city):
-    #load weather's json file
-    res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid.get(city) + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-    data = json.loads(res.text)
+    if city in woeid:
+        #load weather's json file
+        res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid.get(city) + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+        data = json.loads(res.text)
 
-    data = data['query']['results']['channel']
+        data = data['query']['results']['channel']
 
-    atmosphere = data['atmosphere']
-    humidity = atmosphere['humidity']
+        atmosphere = data['atmosphere']
+        humidity = atmosphere['humidity']
 
-    item = data['item']
-    temp = item['condition']['temp']
-    #print results
-    print('溫度：' + temp + '°C' + ' 濕度：' + humidity + '% 天氣狀況：' + item['condition']['text'])
-    TTS('溫度：' + temp + '°C' + ' 濕度：' + humidity + '% 天氣狀況：' + item['condition']['text'],'中文')
-
+        item = data['item']
+        temp = item['condition']['temp']
+        #print results
+        print('溫度：' + temp + '°C' + ' 濕度：' + humidity + '% 天氣狀況：' + item['condition']['text'])
+        TTS('溫度：' + temp + '°C' + ' 濕度：' + humidity + '% 天氣狀況：' + item['condition']['text'],'中文')
+    else:
+        TTS('我不懂','中文')
 def weather_forecast(date,city):
-    #處理dialogflow的Timestamp 
-    json_to_date = dateutil.parser.parse(date, ignoretz=True).strftime("%d %b %Y")
-    print(json_to_date)
-    
-    #load weather's json file
-    res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid.get(city) + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')# + woeid.get(city))
-    data = json.loads(res.text)
-    forecast = data['query']['results']['channel']['item']['forecast']
+    if city in woeid:
+        #處理dialogflow的Timestamp
+        json_to_date = dateutil.parser.parse(date, ignoretz=True).strftime("%d %b %Y")
+        print(json_to_date)
 
-    #print results
-    for i in range(len(forecast)) :
-        if json_to_date == forecast[i]['date']:
-            print('氣溫：' + forecast[i]['high'] + '°C~' + forecast[i]['low'] + '°C 天氣狀況：' + forecast[i]['text'])
-            TTS('氣溫：' + forecast[i]['high'] + '°C~' + forecast[i]['low'] + '°C 天氣狀況：' + forecast[i]['text'],'中文')
+        #load weather's json file
+        res = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + woeid.get(city) + '%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')# + woeid.get(city))
+        data = json.loads(res.text)
+        forecast = data['query']['results']['channel']['item']['forecast']
 
+        #print results
+        for i in range(len(forecast)) :
+            if json_to_date == forecast[i]['date']:
+                print('氣溫：' + forecast[i]['high'] + '°C~' + forecast[i]['low'] + '°C 天氣狀況：' + forecast[i]['text'])
+                TTS('氣溫：' + forecast[i]['high'] + '°C~' + forecast[i]['low'] + '°C 天氣狀況：' + forecast[i]['text'],'中文')
+    else:
+        TTS('我不懂','中文')
+#weather_current('臺中')
